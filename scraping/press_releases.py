d4b083220c8b03781bc194b3a0e52d297a29b7ea
KuBaN658 <garkovski_dmitri@mail.ru>
2024-10-07 21:34:38 +0300

    get raw press-releases

--- scraping/press_releases.py
+++ scraping/press_releases.py
@@ -2,34 +2,54 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+import httpx
+import pandas as pd
 
 
 driver = webdriver.Firefox()
-driver.get("https://cbr.ru/")
+driver.get("https://www.cbr.ru/dkp/mp_dec/")
 
 wait = WebDriverWait(driver, 10)
+content = driver.find_element(By.ID, "tab_content_t1")
+btn = content.find_element(By.TAG_NAME, "button")
 
-el = driver.find_element(By.CSS_SELECTOR, 'a.tab[data-tabs-tab="4"]')
-driver.execute_script("arguments[0].click();", el)
+while btn:
+    btn.click()
+    time.sleep(2)
+    content = driver.find_element(By.ID, "tab_content_t1")
+    
+    try:
+        btn = content.find_element(By.TAG_NAME, "button")
+    except NoSuchElementException:
+        btn = None
 
-print(el.text) 
+data = []
+releases = content.find_elements(By.CLASS_NAME, "previews_day")
+for item in releases:
 
-time.sleep(2)
+    data_item = []
+    date = item.find_element(By.CLASS_NAME, "previews_day-date")
+    link = item.find_element(By.TAG_NAME, "a")
 
-btns = driver.find_elements(By.ID, "_buttonLoadNextEvt")
+    data_item.append(date.text)
+    data_item.append(link.get_attribute('href'))
+    data_item.append(link.text)
+    data.append(data_item)
 
-for el in btns:
-    print(el.text)
+for i, (_, link, _) in enumerate(data):
+    response = httpx.get(link)
+    tree = BeautifulSoup(response.text, 'html.parser')
 
+    release = tree.find_all('div', {'class' : 'landing-text'})[0]
+    data[i].append(release.text)
+    time.sleep(1)
 
-driver.execute_script("arguments[0].click();", btns[1])
-print(btns[1].text)
+df = pd.DataFrame(data, columns=['date', 'link', 'title', 'release'])
+df.to_csv('mp_dec.csv', index=False)
 
-
-
-
-
-
+time.sleep(5)
+driver.quit()
 
