0b81af3eca736ecff10c3d0a8581f580e8cc694d
KuBaN658 <garkovski_dmitri@mail.ru>
2024-10-26 15:57:34 +0300

    add logger

--- scraping/main.py
+++ scraping/main.py
@@ -2,18 +2,25 @@ import os
 import key_rate
 import press_releases
 import preprocessing
+from logger import logger
 
-if not os.path.exists('../data/'):
-    os.mkdir('../data')
 
-print('Получение пресс-релизов...')
-press_releases.get_press_releases()
-print('Готово.')
+def main():
+    if not os.path.exists('../data/'):
+        os.mkdir('../data')
 
-print('Получение ключевых ставок...')
-key_rate.get_key_rate()
-print('Готово.')
+    logger.info('Получение пресс-релизов.')
+    press_releases.get_press_releases()
+    logger.info('Получение пресс-релизов завершено.')
 
-print('Препроцессинг данных...')
-preprocessing.make_preprocessing()
-print('Готово.')
+    logger.info('Получение ключевых ставок.')
+    key_rate.get_key_rate()
+    logger.info('Получение ключевых ставок завершено.')
+
+    logger.info('Подготовка данных.')
+    preprocessing.make_preprocessing()
+    logger.info('Подготовка данных завершена.')
+
+
+if __name__ == '__main__':
+    main()
--- scraping/press_releases.py
+++ scraping/press_releases.py
@@ -2,15 +2,24 @@ import time
 from bs4 import BeautifulSoup
 import httpx
 import pandas as pd
+from logger import logger
 
 
-def get_press_releases():
+def get_press_releases() -> None:
     URL = 'http://www.cbr.ru/Crosscut/NewsList/LoadMore/84035?intOffset=0&extOffset='
     offset = 0
     data = []
 
+    
     while True:
-        page = httpx.get(URL + str(offset))
+        try:
+            page = httpx.get(URL + str(offset))
+
+        except Exception as e:
+            logger.error(e)
+            time.sleep(10)
+            continue
+        
         if len(page.text.strip()) == 0:
             break
 
@@ -27,14 +36,25 @@ def get_press_releases():
                 data_item.append(link.text.strip())
                 data.append(data_item)
 
+        logger.info(f'Получено {offset + len(releases)} ссылок на релизы')
+
         offset += 10
         time.sleep(1)
 
     for i, (_, link, _) in enumerate(data):
-        response = httpx.get(link)
+
+        try:
+            response = httpx.get(link)
+        except Exception as e:
+            logger.error(e)
+            continue
+        
         tree = BeautifulSoup(response.text, 'html.parser')
         release = tree.find_all('div', {'class': 'landing-text'})[0]
         data[i].append(release.text.strip())
+        
+        logger.info(f'Получен текст релиза от {data[i][0]}')
+
         time.sleep(1)
 
     df = pd.DataFrame(data, columns=['date', 'link', 'title', 'release'])
