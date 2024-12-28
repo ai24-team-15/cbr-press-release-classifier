0dac63ccd47c91993e0dbcd421268382561585a2
KuBaN658 <garkovski_dmitri@mail.ru>
2024-12-28 22:02:54 +0300

    fix codestyle

--- frontend/pages/machine_learning/fit.py
+++ frontend/pages/machine_learning/fit.py
@@ -2,76 +2,120 @@ import asyncio
 import streamlit as st
 from tools.api import client
 
+# Заголовок страницы
 st.header("Обучение модели")
+
+# Инициализация пустого словаря для передачи данных в запрос
 payload = {}
 
+# Выбор типа модели с помощью радио-кнопок
 payload["type"] = st.radio("Выберите модель", ["LogisticRegression", "SVC"])
 
+# Подзаголовок для выбора гиперпараметров модели
 st.subheader("Выберите гиперпараметры модели")
+
+# Словарь для хранения значений гиперпараметров
 params = {}
+
+# Выбор значения гиперпараметра C с помощью слайдера
 params["C"] = st.slider("C", 0.0, 10.0, step=0.001, value=1.0, format="%.3f")
+
+# Выбор значения гиперпараметра tol с помощью слайдера
 params["tol"] = st.slider("tol", 1e-6, 1e-3, step=0.000001, format="%.6f", value=1e-4)
 
+# Условия для настройки гиперпараметров для LogisticRegression
 if payload["type"] == "LogisticRegression":
 
+    # Выбор типа регуляризации (penalty)
     params["penalty"] = st.radio("penalty", ["l2", None, "l1", "elasticnet"])
 
+    # Условия для выбора solver в зависимости от выбранной penalty
     if params["penalty"] == "l2":
-        params["solver"] = st.radio("solver", 
-                      ["lbfgs", 
-                       "liblinear", 
-                       "newton-cg", 
-                       "newton-cholesky", 
-                       "sag", 
-                       "saga"])
+        params["solver"] = st.radio(
+            "solver",
+            [
+                "lbfgs",
+                "liblinear",
+                "newton-cg",
+                "newton-cholesky",
+                "sag",
+                "saga"
+            ])
     if params["penalty"] is None:
-        params["solver"] = st.radio("solver", 
-                      ["lbfgs", 
-                       "newton-cg", 
-                       "newton-cholesky", 
-                       "sag", 
-                       "saga"])
+        params["solver"] = st.radio(
+            "solver",
+            [
+                "lbfgs",
+                "newton-cg",
+                "newton-cholesky",
+                "sag",
+                "saga"
+            ])
     if params["penalty"] == "l1":
-        params["solver"] = st.radio("solver", 
-                      ["liblinear", 
-                       "saga"])
+        params["solver"] = st.radio(
+            "solver",
+            [
+                "liblinear",
+                "saga"
+            ])
     if params["penalty"] == "elasticnet":
         params["solver"] = "saga"
 
+    # Дополнительная настройка для solver 'liblinear', добавление гиперпараметра dual
     if params["penalty"] == "l2" and params["solver"] == "liblinear":
         params["dual"] = st.radio("dual", [False, True])
 
-
-        
+    # Выбор максимального числа итераций для оптимизации
     params["max_iter"] = st.slider("max_iter", 100, 1000, step=1, value=100)
 
+    # Добавление гиперпараметра warm_start для solver 'liblinear'
     if params["solver"] == "liblinear":
         params["warm_start"] = st.radio("warm_start", [False, True])
 
+    # Если используется penalty 'elasticnet', добавляется гиперпараметр l1_ratio
     if params["penalty"] == "elasticnet":
         params["l1_ratio"] = st.slider("l1_ratio", 0.0, 1.0, step=0.01, value=0.5)
 else:
+    # Условия для настройки гиперпараметров для SVC
+    # Выбор типа ядра (kernel)
     params["kernel"] = st.radio("kernel", ["rbf", "linear", "poly", "sigmoid"])
+
+    # Если выбрано ядро 'poly', добавляем гиперпараметр degree
     if params["kernel"] == "poly":
         params["degree"] = st.slider("degree", 1, 5, step=1, value=3)
+
+    # Если ядро не линейное, добавляем гиперпараметр gamma
     if params["kernel"] != "linear":
         params["gamma"] = st.radio("gamma", ["scale", "auto"])
+
+    # Для некоторых типов ядер добавляется параметр coef0
     if params["kernel"] in set(["poly", "sigmoid"]):
         params["coef0"] = st.slider("coef0", -1.0, 1.0, step=0.01, value=0.0)
+
+    # Установка параметра shrinking
     params["shrinking"] = st.radio("shrinking", [False, True])
+
+    # Включаем вероятность для всех типов ядра
     params["probability"] = True
 
+# Добавление выбранных гиперпараметров в payload
 payload["hyperparameters"] = params
 
+# Ввод ID модели
 payload["model_id"] = st.text_input("Введите id модели")
 
+# Ввод описания модели
 payload["description"] = st.text_input("Введите описание модели")
 
+# Обработка нажатия кнопки "Обучить модель"
 if st.button("Обучить модель"):
+    # Проверка на наличие ID модели
     if not payload["model_id"]:
-        st.error("Введите id модели")
+        st.error("Введите id модели")  # Если ID не введен, выводится ошибка
     else:
+        # Вызов асинхронного метода fit API с передачей данных
         response = asyncio.run(
             client.fit('/fit', payload)
         )
+        # Вывод статуса ответа от API
         st.warning(response["status"])
--- frontend/pages/machine_learning/predict.py
+++ frontend/pages/machine_learning/predict.py
@@ -1,61 +1,72 @@
 import asyncio
-
 import streamlit as st
 import pandas as pd
-
 from tools.api import client
 
-st.header('Прогнозирование Решения ЦБ')
+# Заголовок страницы
+st.header('Прогнозирование решения ЦБ')
 
-models = asyncio.run(
-    client.get_models('/get_models')
-)
+# Получение доступных моделей с сервера с использованием асинхронного запроса
+models = asyncio.run(client.get_models('/get_models'))
 
+# Составление списка ID моделей
 model_ids = [model['model_id'] for model in models]
 
+# Выбор модели для предсказания с помощью радио-кнопок
 model_id = st.radio('Выберите модель', model_ids)
 
+# Отображение информации о выбранной модели
 st.subheader('Выбрана модель:')
 for model in models:
     if model_id == model['model_id']:
         for key in model:
+            # Если это гиперпараметры модели, отображаем их
             if key == 'hyperparameters':
                 if len(model[key]) > 0:
                     st.markdown(f'`{key}`:')
                 for k in model[key]:
-                    st.markdown(f'* `{k}`: {model[key][k]}') 
+                    st.markdown(f'* `{k}`: {model[key][k]}')
+            # Иначе отображаем стандартные параметры модели
             else:
                 st.markdown(f'`{key}`: {model[key]}')
 
-source = st.radio("Данные для предсказания", 
-         ["Ввести текст пресс-релиза", 
-          "Загрузить txt файл", 
-          "Использовать последний пресс-релиз"])
+# Выбор источника данных для предсказания
+source = st.radio("Данные для предсказания",
+                  ["Ввести текст пресс-релиза",
+                   "Загрузить txt файл",
+                   "Использовать последний пресс-релиз"])
 
+# Инициализация переменной для текста пресс-релиза
 release = None
+
+# Загрузка последнего пресс-релиза из файла CSV
 last_release = pd.read_csv('./data/cbr-press-releases.csv').tail(1)
+
+# Обработка выбранного источника данных
 if source == "Ввести текст пресс-релиза":
-    release = st.text_input('Введите текст пресс-релиза')
+    release = st.text_input('Введите текст пресс-релиза')  # Текстовое поле для ввода
 
 elif source == "Загрузить txt файл":
+    # Загрузка текстового файла с пресс-релизом
     file = st.file_uploader('Загрузите txt файл с текстом пресс-релиза', type='txt')
     if file:
-        release = file.read().decode("utf-8")
+        release = file.read().decode("utf-8")  # Декодирование содержимого файла в строку
 
 elif source == "Использовать последний пресс-релиз":
-    release = last_release['release'].values[0]
+    release = last_release['release'].values[0]  # Используем последний пресс-релиз из файла CSV
 
+# Если текст пресс-релиза был введен или загружен, показываем его на экране
 if release:
     last_release['release'] = release
     st.subheader('Текущий текст пресс-релиза')
-    st.text_area('', release, height=200)
+    st.text_area('', release, height=200)  # Отображение текста пресс-релиза в текстовой области
 
-
+# Кнопка для получения предсказания модели
 if st.button('Получить предсказание'):
-    prediction = asyncio.run(
-        client.predict('/predict', model_id=model_id)
-    )
+    # Асинхронный запрос на предсказание с выбранной моделью
+    prediction = asyncio.run(client.predict('/predict', model_id=model_id))
 
+    # Отображение предсказания модели
     st.subheader('Предсказание модели')
     if prediction['predict'] == -1:
         st.write('На следующем заседании ставка будет снижена.')
