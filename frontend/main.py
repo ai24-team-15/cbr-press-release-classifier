bf999b92db2d0549584f0ed6b3037f9c2a22bfe9
KuBaN658 <garkovski_dmitri@mail.ru>
2024-12-28 20:42:56 +0300

    fix codestyle

--- frontend/main.py
+++ frontend/main.py
@@ -6,26 +6,32 @@ import pickle
 import streamlit as st
 import pandas as pd
 
-from tools.utils import download_file, get_data_for_wordclouds, get_vectors, preprocessing_release
+from tools.utils import download_file, get_data_for_wordclouds, \
+    get_vectors, preprocessing_release
 from tools.config import configure_logging
 
-
+# Настройка логирования
 configure_logging()
 logger = logging.getLogger(__name__)
 
+# Конфигурация страницы Streamlit
 st.set_page_config(
-        page_title="checkpoint 4", 
-    )
+    page_title="Checkpoint 4",
+)
 
+# Проверяем наличие файла с данными. Если его нет, загружаем из S3
 if not os.path.exists('./data/cbr-press-releases.csv'):
-    logger.info("Загрузка данных из s3.")
+    logger.info("Загрузка данных из S3.")
     asyncio.run(
-    download_file(
-        'https://storage.yandexcloud.net/cbr-press-release-classifier/cbr-press-releases.csv',
-        './data/cbr-press-releases.csv'
-    ))   
-    logger.info('Загрузка данных из s3 завершена.')
+        download_file(
+            'https://storage.yandexcloud.net/cbr-press-release-classifier\
+                /cbr-press-releases.csv',
+            './data/cbr-press-releases.csv'
+        )
+    )
+    logger.info('Загрузка данных из S3 завершена.')
 
+# Проверяем наличие файла с данными для облаков слов. Если его нет, создаём
 if not os.path.exists('./data/data_wordclouds.pkl'):
     logger.info('Подготовка данных для построения облаков слов.')
     data = pd.read_csv('./data/cbr-press-releases.csv')
@@ -34,66 +40,73 @@ if not os.path.exists('./data/data_wordclouds.pkl'):
         pickle.dump(data_wordclouds, f)
     logger.info('Подготовка данных для построения облаков слов завершена.')
 
+# Проверяем наличие файла с данными для t-SNE. Если его нет, создаём
 if not os.path.exists('./data/data_tsne.pkl'):
     logger.info('Подготовка данных для построения t-SNE.')
     data = pd.read_csv('./data/cbr-press-releases.csv')
-    data['corpus'] = data.release.map(preprocessing_release)
+    data['corpus'] = data['release'].map(preprocessing_release)
     X_tsne = get_vectors(data)
     with open('./data/data_tsne.pkl', 'wb') as f:
         pickle.dump(X_tsne, f)
     logger.info('Подготовка данных для построения t-SNE завершена.')
 
+# Определяем страницы приложения
+# Главная страница
 title_page = st.Page(
     "pages/title.py",
-    title="О проекте", 
+    title="О проекте",
 )
 
+# Страницы исследовательского анализа
 pie_page = st.Page(
-    "pages/analytics/pie.py", 
-    title="Распределение решений", 
-    )
+    "pages/analytics/pie.py",
+    title="Распределение решений",
+)
 
 dinamic_page = st.Page(
-    "pages/analytics/dinamic.py", 
-    title="Динамика ставки", 
-    )
+    "pages/analytics/dinamic.py",
+    title="Динамика ставки",
+)
 
 length_text_page = st.Page(
-    "pages/analytics/length_text.py", 
-    title="Анализ длины текстов", 
-    )
+    "pages/analytics/length_text.py",
+    title="Анализ длины текстов",
+)
 
 wordcloud_page = st.Page(
-    "pages/analytics/wordcloud.py", 
-    title="Облака слов", 
-    )
+    "pages/analytics/wordcloud.py",
+    title="Облака слов",
+)
 
 tsne_page = st.Page(
-    "pages/analytics/tsne.py", 
-    title="t-SNE визуализация", 
-    )
+    "pages/analytics/tsne.py",
+    title="t-SNE визуализация",
+)
 
+# Страницы машинного обучения
 fit_page = st.Page(
-    "pages/machine_learning/fit.py", 
-    title="Обучение модели", 
-    )
+    "pages/machine_learning/fit.py",
+    title="Обучение модели",
+)
 
 predict_page = st.Page(
-    "pages/machine_learning/predict.py", 
-    title="Предсказание", 
-    )
+    "pages/machine_learning/predict.py",
+    title="Предсказание",
+)
 
+# Настройка навигации между страницами
 pg = st.navigation(
     {
         "Главная": [title_page],
         "Исследовательский анализ": [
-            pie_page, dinamic_page, 
-            length_text_page, 
-            wordcloud_page, 
+            pie_page, dinamic_page,
+            length_text_page,
+            wordcloud_page,
             tsne_page
-            ],
+        ],
         "Машинное обучение": [fit_page, predict_page],
     }
 )
 
+# Запуск выбранной страницы
 pg.run()
--- frontend/tools/utils.py
+++ frontend/tools/utils.py
@@ -1,7 +1,6 @@
 import re
-from typing import Union
+from typing import Union, List, Dict, Set
 from collections import Counter
-import asyncio
 import os
 
 import pandas as pd
@@ -13,8 +12,10 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.manifold import TSNE
 import aiohttp
 
+# Загрузка необходимых ресурсов NLTK
 nltk.download('stopwords')
 
+# Определение цветов для визуализации
 COLORS = {
     'Снижение ставки': '#4D6D7F',
     'Сохранение ставки': '#1A4E1A',
@@ -24,90 +25,158 @@ COLORS = {
     0: '#A03B2A'
 }
 
+# Определение категорий
 CATEGORIAL_NAMES = {
-            -1: 'Снижение ставки', 
-            1: 'Повышение ставки', 
-            0: 'Сохранение ставки'
-        }
+    -1: 'Снижение ставки',
+    1: 'Повышение ставки',
+    0: 'Сохранение ставки'
+}
+
 
 @st.cache_resource
-def preprocessing_release(text: str) -> list[str]:
+def preprocessing_release(text: str) -> List[str]:
+    """
+    Предобработка текста: лемматизация, удаление стоп-слов и символов, не относящихся к кириллице.
+
+    Аргументы:
+        text (str): Исходный текст.
+
+    Возвращает:
+        List[str]: Список обработанных слов.
+    """
     stop_words = set(nltk.corpus.stopwords.words('russian'))
     regex = re.compile('[^а-я ]')
     mystem = Mystem()
 
-    text = text.lower()
-    text = regex.sub(' ', text)
-    text = filter(lambda w: not w.isspace(), mystem.lemmatize(text))
-    text = list(filter(lambda w: w not in stop_words and len(w) != 1, text))
+    text = text.lower()  # Приведение текста к нижнему регистру
+    text = regex.sub(' ', text)  # Удаление всех символов, кроме кириллицы и пробелов
+    text = filter(lambda w: not w.isspace(), mystem.lemmatize(text))  # Лемматизация
+    text = list(filter(lambda w: w not in stop_words and len(w) != 1, text))  # Удаление стоп-слов и коротких слов
     return text
 
+
 @st.cache_data
-def get_preprocess_texts(df: pd.DataFrame, class_cat: Union[int, None]=None) -> list[str]:
+def get_preprocess_texts(df: pd.DataFrame, class_cat: Union[int, None] = None) -> List[str]:
+    """
+    Извлекает и обрабатывает текстовые данные из DataFrame в зависимости от категории.
+
+    Аргументы:
+        df (pd.DataFrame): DataFrame с текстовыми данными.
+        class_cat (Union[int, None], optional): Категория для фильтрации. По умолчанию None.
+
+    Возвращает:
+        List[str]: Список обработанных слов из выбранных текстов.
+    """
     if class_cat is None:
         df = df.release
     else:
         df = df.loc[df.target_categorial == class_cat, 'release']
-        
-    texts_raw = ' '.join(df.to_list())
-    texts = preprocessing_release(texts_raw)
+
+    texts_raw = ' '.join(df.to_list())  # Объединение текстов в одну строку
+    texts = preprocessing_release(texts_raw)  # Предобработка текста
     return texts
 
+
 @st.cache_data
-def get_freq(texts: list[str]) -> dict[str, float]:
-    cnt_words = Counter(texts)
-    num_words = sum(cnt_words.values())
-    cnt_words = {k: v / num_words for k, v in cnt_words.items()}
-    cnt_words = dict(sorted(cnt_words.items(), key=lambda w: -w[1]))
+def get_freq(texts: List[str]) -> Dict[str, float]:
+    """
+    Вычисляет частоту слов в списке текстов.
+
+    Аргументы:
+        texts (List[str]): Список слов.
+
+    Возвращает:
+        Dict[str, float]: Словарь с частотами слов (слово: частота).
+    """
+    cnt_words = Counter(texts)  # Подсчет количества каждого слова
+    num_words = sum(cnt_words.values())  # Общее количество слов
+    cnt_words = {k: v / num_words for k, v in cnt_words.items()}  # Нормализация частот
+    cnt_words = dict(sorted(cnt_words.items(), key=lambda w: -w[1]))  # Сортировка по убыванию частоты
     return cnt_words
 
-@st.cache_data
-def calc_common_words(*cnt_words_dic: dict, top:int=15) -> set[str]:
-    result = set(list(cnt_words_dic[0].keys())[:top])
 
+@st.cache_data
+def calc_common_words(*cnt_words_dic: Dict[str, float], top: int = 15) -> Set[str]:
+    """
+    Вычисляет общие слова среди нескольких словарей с частотами.
+
+    Аргументы:
+        *cnt_words_dic (Dict[str, float]): Словари с частотами слов.
+        top (int, optional): Количество верхних слов для учета. По умолчанию 15.
+
+    Возвращает:
+        Set[str]: Множество общих слов среди словарей.
+    """
+    result = set(list(cnt_words_dic[0].keys())[:top])  # Берем топ-слова из первого словаря
     for dic in cnt_words_dic[1:]:
-        result.intersection_update(set(list(dic.keys())[:top]))
-    
+        result.intersection_update(set(list(dic.keys())[:top]))  # Оставляем только общие слова
+
     return result
 
-@st.cache_resource
-def get_vectors(df):
-    vec = TfidfVectorizer(analyzer=lambda x: x)
-    X = vec.fit_transform(df.corpus)
 
-    tsne = TSNE(n_components=2, random_state=42)
-    X_tsne = tsne.fit_transform(X.toarray())
+@st.cache_resource
+def get_vectors(df: pd.DataFrame) -> np.ndarray:
+    """
+    Генерирует t-SNE векторы для корпуса текстов в DataFrame.
+
+    Аргументы:
+        df (pd.DataFrame): DataFrame с колонкой 'corpus'.
+
+    Возвращает:
+        np.ndarray: Двумерные t-SNE векторы.
+    """
+    vec = TfidfVectorizer(analyzer=lambda x: x)  # Инициализация векторизатора
+    X = vec.fit_transform(df.corpus)  # Преобразование текстов в векторы
+
+    tsne = TSNE(n_components=2, random_state=42)  # Инициализация t-SNE
+    X_tsne = tsne.fit_transform(X.toarray())  # Преобразование в двумерное пространство
 
     return X_tsne
 
 
-async def download_file(url, filename):
+async def download_file(url: str, filename: str) -> None:
+    """
+    Асинхронная загрузка файла с указанного URL и сохранение его локально.
+
+    Аргументы:
+        url (str): URL для загрузки файла.
+        filename (str): Имя файла для сохранения.
+    """
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
-            os.makedirs("data", exist_ok=True)
+            os.makedirs("data", exist_ok=True)  # Создание папки, если ее нет
             with open(filename, 'wb') as f:
                 while True:
-                    chunk = await response.content.read(1024)
+                    chunk = await response.content.read(1024)  # Загрузка файла частями
                     if not chunk:
                         break
                     f.write(chunk)
 
 
-def get_data_for_wordclouds(data):
+def get_data_for_wordclouds(data: pd.DataFrame) -> Dict[str, Union[Dict[int, Dict[str, float]], Set[str]]]:
+    """
+    Обрабатывает данные для генерации частот слов и общих слов для облаков слов.
+
+    Аргументы:
+        data (pd.DataFrame): DataFrame с колонками 'target_categorial' и текстовыми данными.
+
+    Возвращает:
+        Dict[str, Union[Dict[int, Dict[str, float]], Set[str]]]:
+            Словарь с частотами слов, общими словами и частотами для всех классов.
+    """
     frequencies = {}
-    unique_target = data.target_categorial.unique()
-    unique_target = unique_target[~np.isnan(unique_target)]
+    unique_target = data.target_categorial.unique()  # Уникальные категории
+    unique_target = unique_target[~np.isnan(unique_target)]  # Исключение NaN
 
     for target in unique_target:
-        frequencies[target] = get_preprocess_texts(data, target)
+        frequencies[target] = get_preprocess_texts(data, target)  # Предобработка текста для каждой категории
 
-    texts_all_class = get_preprocess_texts(data)
+    texts_all_class = get_preprocess_texts(data)  # Тексты всех категорий
 
     for target in unique_target:
-        frequencies[target] = get_freq(frequencies[target])
+        frequencies[target] = get_freq(frequencies[target])  # Частоты слов для каждой категории
 
-    cnt_words_all_classes = get_freq(texts_all_class)
+    cnt_words_all_classes = get_freq(texts_all_class)  # Частоты слов для всех категорий
+    common_words = calc_common_words(*frequencies.values())  # Общие слова между категориями
 
-    common_words = calc_common_words(*frequencies.values())
-    
     return {'data': frequencies, 'common_words': common_words, 'cnt_words_all_classes': cnt_words_all_classes}
