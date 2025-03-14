cf06dd04c82ef5950c227cfe7b06b06a97952e63
eto-enot <66057023+eto-enot@users.noreply.github.com>
2025-03-14 07:26:05 +0300

    fix ensemble

--- ml/utils.py
+++ ml/utils.py
@@ -1,10 +1,20 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
 import seaborn as sns
-from sklearn.metrics import accuracy_score, f1_score, recall_score, \
-    precision_score, roc_auc_score, confusion_matrix, classification_report
+import pickle
+from matplotlib import pyplot as plt
 from joblib import Parallel, delayed
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
+from sklearn.metrics import (
+    accuracy_score,
+    f1_score,
+    recall_score,
+    precision_score,
+    roc_auc_score,
+    confusion_matrix,
+    classification_report,
+)
 
 
 def _calc_metric(model, X, y, threshold):
@@ -13,21 +23,21 @@ def _calc_metric(model, X, y, threshold):
     y_train = y[:threshold]
 
     model.fit(X_train, y_train)
-    
+
     y_pred = model.predict(X_test[0].reshape(1, -1))
     y_pred_proba = model.predict_proba(X_test[0].reshape(1, -1))
-    
+
     return (y_pred.item(), y_pred_proba)
 
 
 def calc_metrics(X, y, model, *, name, plot=True, calc_jobs=1, **params):
     """
-    Функция для тестирования наших моделей. 
-    Зададим начальный порог и будем обучать, 
-    модель на наблюдениях до порога, 
-    а тестировать на одном наблюдении после. 
-    Двигая порог протестируем нашу модель. 
-    И потом сравним с истинными ответами. 
+    Функция для тестирования наших моделей.
+    Зададим начальный порог и будем обучать,
+    модель на наблюдениях до порога,
+    а тестировать на одном наблюдении после.
+    Двигая порог протестируем нашу модель.
+    И потом сравним с истинными ответами.
     Качество всех наших моделей будем записывать в metrics.csv
     """
 
@@ -38,35 +48,62 @@ def calc_metrics(X, y, model, *, name, plot=True, calc_jobs=1, **params):
         delayed(_calc_metric)(model, X, y, threshold)
         for threshold in range(30, y.shape[0])
     )
-    
+
     y_preds = [x[0] for x in results]
     y_preds_proba = [x[1] for x in results]
-    
+
     acc = accuracy_score(y[30:], y_preds)
-    f1 = f1_score(y[30:], y_preds, average='macro')
-    recall = recall_score(y[30:], y_preds, average='macro')
-    precision = precision_score(y[30:], y_preds, average='macro')
-    roc_auc_ovr = roc_auc_score(y[30:], np.concatenate(y_preds_proba, axis=0), average='macro', multi_class='ovr')
-    roc_auc_ovo = roc_auc_score(y[30:], np.concatenate(y_preds_proba, axis=0), average='macro', multi_class='ovo')
+    f1 = f1_score(y[30:], y_preds, average="macro")
+    recall = recall_score(y[30:], y_preds, average="macro")
+    precision = precision_score(y[30:], y_preds, average="macro")
+    roc_auc_ovr = roc_auc_score(
+        y[30:],
+        np.concatenate(y_preds_proba, axis=0),
+        average="macro",
+        multi_class="ovr",
+    )
+    roc_auc_ovo = roc_auc_score(
+        y[30:],
+        np.concatenate(y_preds_proba, axis=0),
+        average="macro",
+        multi_class="ovo",
+    )
     if plot:
         print(classification_report(y[30:], y_preds))
         cm = confusion_matrix(y[30:], y_preds)
-        cm = pd.DataFrame(cm, index=['-1', '0', '1'], columns=['-1', '0', 1])
-        sns.heatmap(cm, annot=True, fmt='d')
+        cm = pd.DataFrame(cm, index=["-1", "0", "1"], columns=["-1", "0", 1])
+        sns.heatmap(cm, annot=True, fmt="d")
         plt.title("Confusion matrix")
-        plt.ylabel('True label')
-        plt.xlabel('Predicted label')
+        plt.ylabel("True label")
+        plt.xlabel("Predicted label")
         plt.show()
 
-        metrics = pd.DataFrame({
-            'accuracy': acc, 
-            'f1': f1, 
-            'recall': recall, 
-            'precision': precision, 
-            'roc_auc_ovr': roc_auc_ovr,
-            'roc_auc_ovo': roc_auc_ovo,
-            }, index=[name])
+        metrics = pd.DataFrame(
+            {
+                "accuracy": acc,
+                "f1": f1,
+                "recall": recall,
+                "precision": precision,
+                "roc_auc_ovr": roc_auc_ovr,
+                "roc_auc_ovo": roc_auc_ovo,
+            },
+            index=[name],
+        )
         return metrics, model
     else:
         return roc_auc_ovo.item(), model
-    
+
+
+class PrefitTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, transformer):
+        if isinstance(transformer, BaseEstimator):
+            check_is_fitted(transformer)
+            self.transformer = pickle.dumps(transformer)
+        else:
+            self.transformer = transformer
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return pickle.loads(self.transformer).transform(X)
